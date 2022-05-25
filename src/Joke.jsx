253fdb7895e2011737c07586709bc2ad194da5eb
priyamaneja2002 <priyamaneja2002@gmail.com>
2022-05-25 19:05:34 +0530

    trial n error 1

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -55,7 +55,7 @@ export default class Joke extends Component {
       <div className="Joke">
           <div className="Joke-buttons">
               <i className="fas fa-arrow-up" onClick={this.props.upvote}></i>
-              <span className='Joke-votes' style={{borderColor: this.getColor()}}>{this.props.votes}</span>
+              <span className='Joke-votes' /*style={{borderColor: this.getColor()}}*/>{this.props.votes}</span>
               <i className="fas fa-arrow-down" onClick={this.props.downvote}></i>
           </div>
           <div className="Joke-text">
